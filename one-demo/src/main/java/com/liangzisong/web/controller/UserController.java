4a9538ab4dfc5723eb8af9b141766086224de4a0
liang <18462146261@163.com>
2019-08-26 15:54:37 +0800

    first commit

---
+++ one-demo/src/main/java/com/liangzisong/web/controller/UserController.java
@@ -0,0 +1,110 @@
+package com.liangzisong.web.controller;//
+//
+//
+//
+
+////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//         佛祖保佑       永无BUG     永不修改                       //
+////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//让需求简单一点，心灵就会更轻松一点；
+//让外表简单一点，本色就会更接近一点；
+//让过程简单一点，内涵就会更丰富一点；
+//
+//
+//
+
+
+import com.liangzisong.dto.User;
+import com.liangzisong.dto.UserQueryCondition;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.codehaus.jackson.map.annotate.JsonView;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright (C), 2002-2019, 山东沃然网络科技有限公司
+ * FileName: UserController
+ * <p>
+ * Description: 用户controller
+ *
+ * @author 如果这段代码非常棒就是梁子松写的
+ * 如果这代码挺差劲那么我也不知道是谁写的
+ * @version 1.0.0
+ * @create 2019/8/26 14:30
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping
+    @JsonView({User.UserSimpleView.class})
+    public List<User> query(UserQueryCondition condition){
+        System.out.println(ReflectionToStringBuilder.toString(condition, ToStringStyle.MULTI_LINE_STYLE));
+        List<User> list = new ArrayList<>();
+        list.add(new User());
+        list.add(new User());
+        list.add(new User());
+
+        return list;
+    }
+
+
+    @GetMapping("{id:\\d+}")
+    @JsonView({User.UserDetailView.class})
+    public User getInfo(@PathVariable String id){
+        System.out.println("id = " + id);
+        User user = new User();
+        user.setUsername("tom");
+        return user ;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        System.out.println("user = " + user);
+        user.setId("1");
+        return user;
+    }
+
+    @PutMapping("{id:\\d+}")
+    public User updateUser(@RequestBody User user){
+        user.setId("1");
+        return user ;
+    }
+
+    @DeleteMapping("{id:\\d+}")
+    public void delete(@PathVariable String id){
+        System.out.println("id = " + id);
+    }
+
+
+
+}
---
+++ one-demo/src/test/java/com/liangzisong/web/controller/UserControllerTest.java
@@ -0,0 +1,163 @@
+package com.liangzisong.web.controller;//
+//
+//
+//
+
+////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//         佛祖保佑       永无BUG     永不修改                       //
+////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//让需求简单一点，心灵就会更轻松一点；
+//让外表简单一点，本色就会更接近一点；
+//让过程简单一点，内涵就会更丰富一点；
+//
+//
+//
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Copyright (C), 2002-2019, 山东沃然网络科技有限公司
+ * FileName: UserController
+ * <p>
+ * Description: 用户controller
+ *
+ * @author 如果这段代码非常棒就是梁子松写的
+ * 如果这代码挺差劲那么我也不知道是谁写的
+ * @version 1.0.0
+ * @create 2019/8/26 14:30
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void whenQuerySuccess() throws Exception {
+        String contentAsString = mockMvc.perform(
+                get("/user")
+                        .param("username", "jojo")
+                        .param("age", "18")
+                        .param("ageTo", "60")
+                        .param("xxx", "yyy")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(3))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println("contentAsString = " + contentAsString);
+    }
+
+
+    @Test
+    public void whenGenInfoSuccess() throws Exception {
+        String contentAsString = mockMvc.perform(
+                get("/user/1")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("tom"))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println("contentAsString = " + contentAsString);
+    }
+
+    @Test
+    public void whenGenFail() throws Exception {
+        mockMvc.perform(
+                get("/user/a")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        )
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void whenCreateSuccess() throws Exception {
+        String content = "{\"username\":\"tom\",\"password\":null,\"birthday\":"+System.currentTimeMillis()+"}";
+        String contentAsString = mockMvc.perform(
+                post("/user")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(content)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println("contentAsString = " + contentAsString);
+    }
+
+
+    @Test
+    public void whenUpdateSuccess() throws Exception {
+
+        Date date = new Date(LocalDateTime.now().plusYears(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+        System.out.println(date.getTime());
+        String content = "{\"id\":\"1\", \"username\":\"tom\",\"password\":null,\"birthday\":"+date.getTime()+"}";
+        String reuslt = mockMvc.perform(put("/user/1").contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andReturn().getResponse().getContentAsString();
+
+        System.out.println(reuslt);
+    }
+
+    @Test
+    public void whenDeleteSuccess() throws Exception {
+        String contentAsString = mockMvc.perform(delete("/user/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+//        System.out.println("contentAsString = " + contentAsString);
+    }
+
+}
