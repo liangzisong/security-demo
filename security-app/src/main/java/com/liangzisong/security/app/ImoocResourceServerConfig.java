8aba7018823c1bdf8241112dca2b09954e137056
liangzisong <18462146261@163.com>
2019-09-07 07:18:19 +0800

    用户名密码登录现以使用token方式，没有完成验证码

--- security-app/src/main/java/com/liangzisong/security/app/ImoocResourceServerConfig.java
+++ security-app/src/main/java/com/liangzisong/security/app/ImoocResourceServerConfig.java
@@ -37,8 +37,18 @@ package com.liangzisong.security.app;//
 //
 
 
+import com.liangzisong.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.liangzisong.security.core.properties.SecurityConstants;
+import com.liangzisong.security.core.properties.SecurityProperties;
+import com.liangzisong.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 import org.springframework.stereotype.Component;
 
 /**
@@ -51,6 +61,68 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @EnableResourceServer
-public class ImoocResourceServerConfig {
+public class ImoocResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
 
+    @Autowired
+    protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer liangzisongSocialConfig;
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    //安全配置
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(imoocAuthenticationSuccessHandler)
+                .failureHandler(imoocAuthenticationFailureHandler);
+
+
+        http
+                //图形验证码
+//                .apply(validateCodeSecurityConfig)
+                //短信
+//                .and().apply(smsCodeAuthenticationSecurityConfig)
+                //第三方认证
+//                .and().apply(liangzisongSocialConfig)
+                //对请求授权
+//                .and()
+                .authorizeRequests()
+                //当访问下面的登录页面不需要认证
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        //登录
+                        securityProperties.getBrowser().getLoginPage(),
+                        //注册页面
+                        securityProperties.getBrowser().getSigUpUrl(),
+                        //验证码
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                        //session失效链接
+                        SecurityConstants.DEFAULT_SESSION_INVALID_URL,
+                        //退出页面
+                        securityProperties.getBrowser().getSignOutUrl(),
+                        //真正注册逻辑
+                        "/user/regist"
+
+                )
+                //任何请求 都需要身份认证
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                //暂时把csrf 忽略掉
+                .and().csrf().disable();
+    }
 }
--- security-app/src/main/java/com/liangzisong/security/app/authentication/ImoocAuthenticationSuccessHandler.java
+++ security-app/src/main/java/com/liangzisong/security/app/authentication/ImoocAuthenticationSuccessHandler.java
@@ -40,10 +40,18 @@ package com.liangzisong.security.app.authentication;//
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.liangzisong.security.core.properties.LoginResponseType;
 import com.liangzisong.security.core.properties.SecurityProperties;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -74,6 +82,12 @@ public class ImoocAuthenticationSuccessHandler extends SavedRequestAwareAuthenti
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
     /*
      * (non-Javadoc)
      *
@@ -87,13 +101,64 @@ public class ImoocAuthenticationSuccessHandler extends SavedRequestAwareAuthenti
                                         Authentication authentication) throws IOException, ServletException {
 
         logger.info("登录成功");
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
+        }
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
 
-        if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        } else {
-            super.onAuthenticationSuccess(request, response, authentication);
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在=" + clientId);
+            //如果secret 不存在
+        } else if (!StringUtils.equals(clientDetails.getClientSecret(),clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配=" + clientSecret);
         }
 
+        //这的clientDetails.getScope()可以从数据库里面拿，我就不拿了，直接用传过来的scope
+        //这的grantType 写的是自己自定义的东西，我这里写custom了
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        //拿到令牌
+        OAuth2AccessToken accessToken = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(objectMapper.writeValueAsString(accessToken));
+
+    }
+
+
+    /**
+     * 截取用户信息
+     * @param header 请求头
+     * @param request 请求
+     * @return
+     * @throws IOException
+     */
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException var7) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, "UTF-8");
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        } else {
+            return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+        }
     }
 }
